a676b0754c330402bd1f36099274c198ebeae2be
Musheeb Affan <musheebaffan@Musheebs-MacBook-Air.local>
2026-02-22 16:57:32 +0530

    feat: protected route practice and implementation

---
+++ src/components/Confidential/Confidential.jsx
@@ -0,0 +1,10 @@
+export default function Confidential() {
+  return (
+    <div>
+      <h1>
+        Confidential Page - You are on this page which means you are
+        authenticated user
+      </h1>
+    </div>
+  );
+}
---
+++ src/components/ProtectedRoutes/ProtectedRoutes.jsx
@@ -0,0 +1,6 @@
+import { Outlet, Navigate } from "react-router-dom";
+
+export default function ProtectedRoutes() {
+  const user = false;
+  return user ? <Outlet /> : <Navigate to="/login" />;
+}
